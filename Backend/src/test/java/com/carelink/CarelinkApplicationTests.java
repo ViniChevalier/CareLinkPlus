95e6a73331f61188e218c9c8287d2cca31aefcf4
Vinicius <vini-@live.nl>
2025-07-06 12:55:31 +0100

    Refactor CarelinkApplicationTests to use BlobServiceClient mock instead of AzureBlobService

--- Backend/src/test/java/com/carelink/CarelinkApplicationTests.java
+++ Backend/src/test/java/com/carelink/CarelinkApplicationTests.java
@@ -1,6 +1,6 @@
 package com.carelink;
 
-import com.carelink.medicalhistory.service.AzureBlobService;
+import com.azure.storage.blob.BlobServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -8,10 +8,10 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class CarelinkApplicationTests {
+class CarelinkApplicationTests {
 
     @MockBean
-    private AzureBlobService azureBlobService;
+    private BlobServiceClient blobServiceClient;
 
     @Test
     void contextLoads() {
