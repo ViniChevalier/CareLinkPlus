82601c9a49ffdeac0a1203189b51ab66a30af1d5
Vinicius <vini-@live.nl>
2025-07-06 12:53:38 +0100

    Make CarelinkApplicationTests class public and add AzureBlobService mock

--- Backend/src/test/java/com/carelink/CarelinkApplicationTests.java
+++ Backend/src/test/java/com/carelink/CarelinkApplicationTests.java
@@ -1,12 +1,17 @@
 package com.carelink;
 
+import com.carelink.medicalhistory.service.AzureBlobService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class CarelinkApplicationTests {
+public class CarelinkApplicationTests {
+
+    @MockBean
+    private AzureBlobService azureBlobService;
 
     @Test
     void contextLoads() {
